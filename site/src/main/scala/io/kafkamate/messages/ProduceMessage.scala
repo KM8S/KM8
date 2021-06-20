af8ff0faaeaf3549016feccafb4916682f896a91
Ciprian Sofronia <ciprian.sofronia@gmail.com>
2021-06-20 20:48:50 +0300

    Update producer message

--- site/src/main/scala/io/kafkamate/messages/ProduceMessage.scala
+++ site/src/main/scala/io/kafkamate/messages/ProduceMessage.scala
@@ -107,7 +107,7 @@ import common._
       br(),
       div(
         className := "card",
-        div(className := "card-header", "Add message"),
+        div(className := "card-header", "Publish data using STRING message format"),
         div(
           className := "card-body",
           addMessageForm()
